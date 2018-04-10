0c3e2d845930c68d4525ac09980fee2907bf649f
tkielar <tomasz.kielar@outlook.com>
2018-04-10 21:50:56 +0200

    First, basic implementation

---
+++ Tkielar.Utils.Memoize/Memoize.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Tkielar.Utils.Memoize
+{
+    public static class Memoize
+    {
+        public static Func<TArg1, TResult> Create<TArg1, TResult>(Func<TArg1, TResult> func)
+        {
+            var cache = new ConcurrentDictionary<TArg1, TResult>();
+            return arg => cache.GetOrAdd(arg, func);
+        }
+    }
+}
